2d445f2a8aec01c293b4dd7319f5ee418dd41e7b
matsmy <101180563+matsmy@users.noreply.github.com>
2022-04-05 09:28:41 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+
+      for(int y=0; y<=5;y++){
+        for(int x=0; x<y;x++)
+          {
+            System.out.print('*');
+          }
+        System.out.println();
+      }
+    
+  }
+}
